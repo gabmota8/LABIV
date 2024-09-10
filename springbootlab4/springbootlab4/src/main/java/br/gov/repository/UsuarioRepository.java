09a860def43eb7b5a751811567568fc16eb37bef
Gabriel Mota <gabmota8@hotmail.com>
2024-09-10 23:00:33 +0000

    clone do repositorio mineda

--- springbootlab4/springbootlab4/src/main/java/br/gov/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
